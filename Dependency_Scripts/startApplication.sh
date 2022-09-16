d16a54c5ce7237bf78847e6dea6d259c0c3d276b
choudharyjayant <104073223+choudharyjayant@users.noreply.github.com>
2022-09-16 15:12:28 +0530

    Update startApplication.sh

--- Dependency_Scripts/startApplication.sh
+++ Dependency_Scripts/startApplication.sh
@@ -1,2 +1,4 @@
+mkdir /home/ubuntu/order_service
+cp -r /home/ubuntu/order /home/ubuntu/order_service
 cd /home/ubuntu/order_service/target/
 nohup java -jar jb-hello-world-maven-0.2.0.jar  > /dev/null 2>&1 &
