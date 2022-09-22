aeb20c61f1a6a14710443c588d44a36f4f81fc0c
choudharyjayant <104073223+choudharyjayant@users.noreply.github.com>
2022-09-22 13:41:38 +0530

    Update rmBeforeInstall.sh

--- Dependency_Scripts/rmBeforeInstall.sh
+++ Dependency_Scripts/rmBeforeInstall.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-rm -rf  /home/ubuntu//var/lib/jenkins/workspace/jenkins-gradle/*
+sudo rm -rf  /home/ubuntu//var/lib/jenkins/workspace/jenkins-gradle/*
 echo "I can "write" without double quotes" >> file
